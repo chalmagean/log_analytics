15f83a99e0718818051a551ed32e19e5bddd2d66
Cezar Halmagean <cezar@mixandgo.com>
2016-07-21 09:43:53 +0300

    Add feature spec

--- spec/log_analytics_spec.rb
+++ spec/log_analytics_spec.rb
@@ -1,11 +1,20 @@
 require 'spec_helper'
 
 describe LogAnalytics do
-  it 'has a version number' do
-    expect(LogAnalytics::VERSION).not_to be nil
-  end
+  it 'reports the number of unique views for each path' do
+    log_file = File.dirname(__FILE__) + '/fixtures/webserver.log'
+    expected = <<EOS
+/help_page/1 23 unique views
+/contact 23 unique views
+/home 23 unique views
+/index 23 unique views
+/about/2 22 unique views
+/about 21 unique views
+EOS
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+    parser = LogAnalytics::Parser.new(log_file)
+
+    formatter = LogAnalytics::UniqueViewFormatter.new
+    expect { parser.report(formatter) }.to output(expected).to_stdout
   end
 end
